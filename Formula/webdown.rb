637b807b56e6b980a91535d849b581f10c5022d8
Travis Cole <11240+kelp@users.noreply.github.com>
2025-03-27 10:50:53 -0700

    Simplify installation to direct pip install

--- Formula/webdown.rb
+++ Formula/webdown.rb
@@ -1,6 +1,4 @@
 class Webdown < Formula
-  include Language::Python::Virtualenv
-
   desc "CLI tool for converting web pages to clean, readable Markdown format"
   homepage "https://github.com/kelp/webdown"
   url "https://files.pythonhosted.org/packages/source/w/webdown/webdown-0.6.3.tar.gz"
@@ -9,58 +7,11 @@ class Webdown < Formula
 
   depends_on "python"
 
-  resource "beautifulsoup4" do
-    url "https://files.pythonhosted.org/packages/source/b/beautifulsoup4/beautifulsoup4-4.13.3.tar.gz"
-    sha256 "1bd32405dacc920b42b83ba01644747ed77456a65760e285fbc47633ceddaf8b"
-  end
-
-  resource "certifi" do
-    url "https://files.pythonhosted.org/packages/source/c/certifi/certifi-2024.2.2.tar.gz"
-    sha256 "0569859f95fc761b18b45ef421b1290a0f65f147e92a1e5eb3e635f9a5e4e66f"
-  end
-
-  resource "charset-normalizer" do
-    url "https://files.pythonhosted.org/packages/source/c/charset-normalizer/charset-normalizer-3.3.2.tar.gz"
-    sha256 "f30c3cb33b24454a82faecaf01b19c18562b1e89558fb6c56de4d9118a032fd5"
-  end
-
-  resource "html2text" do
-    url "https://files.pythonhosted.org/packages/source/h/html2text/html2text-2024.2.26.tar.gz"
-    sha256 "05f8e367d15aaabc96415376776cdd11afd5127a77fce6e36afc60c563ca2c32"
-  end
-
-  resource "idna" do
-    url "https://files.pythonhosted.org/packages/source/i/idna/idna-3.6.tar.gz"
-    sha256 "9ecdbbd083b06798ae1e86adcbfe8ab1479cf864e4ee30fe4e46a003d12491ca"
-  end
-
-  resource "requests" do
-    url "https://files.pythonhosted.org/packages/source/r/requests/requests-2.32.3.tar.gz"
-    sha256 "55365417734eb18255590a9ff9eb97e9e1da868d4ccd6402399eaf68af20a760"
-  end
-
-  resource "soupsieve" do
-    url "https://files.pythonhosted.org/packages/source/s/soupsieve/soupsieve-2.5.tar.gz"
-    sha256 "5663d5a7b3bfaeee0bc4372e7fc48f9cff4940b3eec54a6451cc5299f1097690"
-  end
-
-  resource "tqdm" do
-    url "https://files.pythonhosted.org/packages/source/t/tqdm/tqdm-4.67.1.tar.gz"
-    sha256 "f8aef9c52c08c13a65f30ea34f4e5aac3fd1a34959879d7e59e63027286627f2"
-  end
-
-  resource "typing-extensions" do
-    url "https://files.pythonhosted.org/packages/source/t/typing-extensions/typing_extensions-4.13.0.tar.gz"
-    sha256 "0a4ac55a5820789d87e297727d229866c9650f6521b64206413c4fbada24d95b"
-  end
-
-  resource "urllib3" do
-    url "https://files.pythonhosted.org/packages/source/u/urllib3/urllib3-1.26.18.tar.gz"
-    sha256 "f8ecc1bba5667413457c529ab955bf8c67b45db799d159066261719e328580a0"
-  end
-
   def install
-    virtualenv_install_with_resources
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{Language::Python.major_minor_version "python3"}/site-packages"
+    system "python3", "-m", "pip", "install", "--prefix=#{libexec}", "."
+    bin.install Dir["#{libexec}/bin/*"]
+    bin.env_script_all_files(libexec/"bin", PYTHONPATH: ENV["PYTHONPATH"])
   end
 
   test do
