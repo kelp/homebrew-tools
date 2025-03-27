e560aa788a34a075bfb7b11800064c0dea7b07b4
Travis Cole <11240+kelp@users.noreply.github.com>
2025-03-27 10:09:40 -0700

    Fix dependency URLs to use canonical PyPI source URLs

--- Formula/webdown.rb
+++ Formula/webdown.rb
@@ -7,57 +7,57 @@ class Webdown < Formula
   sha256 "9b11b36ebc19ea160b7418f4102483085bd4d092f8e3e99332872b6511a1e72b"
   license "MIT"
 
-  depends_on "python@3.10" => :recommended
-  depends_on "python@3.11" => :optional
-  depends_on "python@3.12" => :optional
-  depends_on "python@3.13" => :optional
-  
-  # Make sure we have a Python 3.10+ available
-  depends_on "python"
+  depends_on "python@3.10"
 
   resource "beautifulsoup4" do
-    url "https://files.pythonhosted.org/packages/af/0b/44c39cf3b18a9280950ad63a579ce395dda4c32193ee9da7ff0aed547094/beautifulsoup4-4.13.3.tar.gz"
+    url "https://pypi.org/packages/source/b/beautifulsoup4/beautifulsoup4-4.13.3.tar.gz"
     sha256 "1bd32405dacc920b42b83ba01644747ed77456a65760e285fbc47633ceddaf8b"
   end
 
   resource "html2text" do
-    url "https://files.pythonhosted.org/packages/1e/8e/86f964b76486a79bc659eb2a151b485c80a6cb5c11df538d2fa24d0c46f7/html2text-2024.2.26.tar.gz"
+    url "https://pypi.org/packages/source/h/html2text/html2text-2024.2.26.tar.gz"
     sha256 "05f8e367d15aaabc96415376776cdd11afd5127a77fce6e36afc60c563ca2c32"
   end
 
   resource "requests" do
-    url "https://files.pythonhosted.org/packages/9f/34/6d5a76ee47f3ce69be59c9eb339418fe2846c6e87c5510866004367f8c63/requests-2.32.3.tar.gz"
+    url "https://pypi.org/packages/source/r/requests/requests-2.32.3.tar.gz"
     sha256 "55365417734eb18255590a9ff9eb97e9e1da868d4ccd6402399eaf68af20a760"
   end
 
   resource "tqdm" do
-    url "https://files.pythonhosted.org/packages/5c/97/80bbb350ba028f8749be522f6a3d8a8bb25953be1ece1efec9b723c3a888/tqdm-4.67.1.tar.gz"
+    url "https://pypi.org/packages/source/t/tqdm/tqdm-4.67.1.tar.gz"
     sha256 "f8aef9c52c08c13a65f30ea34f4e5aac3fd1a34959879d7e59e63027286627f2"
   end
 
   resource "soupsieve" do
-    url "https://files.pythonhosted.org/packages/ce/21/952a240de1c196c7e3fbcd4e559681f0419b1280c617db21157a0390717b/soupsieve-2.5.tar.gz"
+    url "https://pypi.org/packages/source/s/soupsieve/soupsieve-2.5.tar.gz"
     sha256 "5663d5a7b3bfaeee0bc4372e7fc48f9cff4940b3eec54a6451cc5299f1097690"
   end
 
   resource "charset-normalizer" do
-    url "https://files.pythonhosted.org/packages/63/09/c1bc53dab74b1816a00d8d030de5bf98f724c52c1635e07681d312f20be8/charset-normalizer-3.3.2.tar.gz"
+    url "https://pypi.org/packages/source/c/charset-normalizer/charset-normalizer-3.3.2.tar.gz"
     sha256 "f30c3cb33b24454a82faecaf01b19c18562b1e89558fb6c56de4d9118a032fd5"
   end
 
   resource "certifi" do
-    url "https://files.pythonhosted.org/packages/71/da/e94e26401b62acd6d91df2b52954aceb7f561743aa5ccc32152886c76c96/certifi-2024.2.2.tar.gz"
+    url "https://pypi.org/packages/source/c/certifi/certifi-2024.2.2.tar.gz"
     sha256 "0569859f95fc761b18b45ef421b1290a0f65f147e92a1e5eb3e635f9a5e4e66f"
   end
 
   resource "idna" do
-    url "https://files.pythonhosted.org/packages/bf/3f/ea4b9117521a1e9c50344b909be7886dd00a519552724809bb1f486986c2/idna-3.6.tar.gz"
+    url "https://pypi.org/packages/source/i/idna/idna-3.6.tar.gz"
     sha256 "9ecdbbd083b06798ae1e86adcbfe8ab1479cf864e4ee30fe4e46a003d12491ca"
   end
 
   resource "urllib3" do
-    url "https://files.pythonhosted.org/packages/0c/39/64487bf07df2ed854cc06078c27c0d0abc59bd27b32232980e513293d372/urllib3-1.26.18.tar.gz"
+    url "https://pypi.org/packages/source/u/urllib3/urllib3-1.26.18.tar.gz"
     sha256 "f8ecc1bba5667413457c529ab955bf8c67b45db799d159066261719e328580a0"
+  end
+
+  # Add typing-extensions dependency required by beautifulsoup4
+  resource "typing-extensions" do
+    url "https://pypi.org/packages/source/t/typing-extensions/typing_extensions-4.13.0.tar.gz"
+    sha256 "b0abd7c89e8fb96f98db96d2e9a392a657e9ed6b38a70031375c9f9ba33c7df2"
   end
 
   def install
